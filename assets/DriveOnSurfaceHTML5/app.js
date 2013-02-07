d2ac38aa30bf4d96225c089dd6762343a19ad27b
Antoine Pultier <antoine@blogjaune.fr>
2013-02-07 22:36:02 +0100

    Ajout du travail de laurène, sisi !

---
+++ assets/DriveOnSurfaceHTML5/app.js
@@ -0,0 +1,47 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: user
+ * Date: 11/01/13
+ * Time: 11:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+Ext.application({
+    name: 'DriveOnSurface',
+
+    views: ['Home', 'Kart', 'PlayMode'],
+    controllers: ["DriveController"],
+    //stores: ["Picture"],
+    //models: ["Picture"],
+
+    stop : new Boolean(false),
+    iskartselected : new Boolean(false),
+    socket : io.connect('http://192.168.1.11:3333/'),
+    launch : function(){
+
+        DriveOnSurface.app.socket.on('connect',function(){
+            console.log("connected server");
+            //DriveOnSurface.app.socket.emit("truc", 'canard');
+        });
+
+        DriveOnSurface.app.socket.on('disconnect',function(){
+            console.log("server closed");
+        });
+
+        DriveOnSurface.app.socket.on('message',function(data) {
+            console.log('Received a message from the server: ',data);
+        });
+        var home = {
+            xtype: "home"
+        };
+        var kart = {
+            xtype: "kart"
+        };
+
+        var play = {
+            xtype:"play"
+        };
+        Ext.Viewport.add([home, kart, play]);
+
+    }
+});
---
+++ assets/DriveOnSurfaceHTML5/app/view/Kart.js
@@ -0,0 +1,169 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: user
+ * Date: 11/01/13
+ * Time: 15:02
+ * To change this template use File | Settings | File Templates.
+ */
+Ext.define('DriveOnSurface.view.Kart', {
+    extend:'Ext.Container',
+    alias: "widget.kart",
+    initialize: function(){
+        this.callParent(arguments);
+
+        /*var carousel = new Ext.Carousel({
+            listeners: {
+            activate: function(carousel, newActiveItem, oldActiveItem, options) {
+
+                    var items = [];
+                    items.push({
+                            xtype: 'myimage',
+                            src: 'resources/mario-kart-ds-nintendo-ds-214.jpg'
+                        });
+                    items.push({
+                        xtype: 'myimage',
+                        src: 'resources/mario-kart-ds-nintendo-ds-206.jpg'
+                    });
+                    items.push({
+                        xtype: 'myimage',
+                        src: 'resources/mario-kart-ds-nintendo-ds-208.jpg'
+                    });
+                    items.push({
+                        xtype: 'myimage',
+                        src: 'resources/mario-kart-ds-nintendo-ds-216.jpg'
+                    });
+
+                }
+            }
+        });*/
+
+
+        var rouge = {
+            xtype: 'button',
+            id: 'rouge',
+            //text: 'Rouge',
+            height: 120,
+            //ui:'plain',
+            html: '<div><img src=\'resources/rouge.jpg\' width = "100" heigth = "100" ></div>',
+            //handler : this.onChooseKartRouge(),
+            listeners : {
+                tap : function() {
+                    var text = "Tu as choisi le kart rouge" ;
+                    DriveOnSurface.app.iskartselected = true;
+                    console.log(text);
+                    DriveOnSurface.app.socket.emit("kart", "Red");
+                    this.fireEvent("chooseKartRouge", this);
+
+                }
+            },
+            scope: this
+        };
+        var vert = {
+            xtype: 'button',
+            id: 'vert',
+            //text: 'vert',
+            height: 120,
+            //ui:'plain',
+            html: '<div><img src=\'resources/vert.jpg\' width = "100" heigth = "100" ></div>',
+            //handler : this.onVertTap(),
+            listeners : {
+                tap : function() {
+                    var text = "Tu as choisi le kart vert" ;
+                    DriveOnSurface.app.iskartselected = true;
+                    console.log(text);
+                    DriveOnSurface.app.socket.emit("kart", "Green");
+                    this.fireEvent("chooseKartVert", this);
+                }
+            },
+            scope: this
+        };
+        var bleu = {
+            xtype: 'button',
+            id: 'bleu',
+            //text: 'bleu',
+            height: 120,
+            //ui:'plain',
+            html: '<div><img src=\'resources/bleu.jpg\' width = "100" heigth = "100" ></div>',
+
+            //handler : this.onChooseKartBleu(),
+            listeners : {
+                tap : function() {
+                    var text = "Tu as choisi le kart bleu" ;
+                    DriveOnSurface.app.iskartselected = true;
+                    console.log(text);
+                    DriveOnSurface.app.socket.emit("kart", "Blue");
+                    this.fireEvent("chooseKartBleu", this);
+                }
+            },
+            scope: this
+            };
+        var jaune = new Ext.Button({
+            //xtype: 'button',
+            id: 'jaune',
+            title: 'jaune',
+            //text: 'jaune',
+            height: 120,
+            //ui:'plain',
+            html: '<div><img src=\'resources/jaune.jpg\' width = "100" heigth = "100" ></div>',
+           // handler : this.onChooseKartJaune(),
+            listeners : {
+                tap : function() {
+                    var text = "Tu as choisi le kart jaune" ;
+                    DriveOnSurface.app.iskartselected = true;
+                    console.log(text);
+                    DriveOnSurface.app.socket.emit("kart", "Yellow");
+                    this.fireEvent("chooseKartJaune", this);
+                }
+            },
+            scope: this
+        });
+
+        var paneltext = {
+            xtype: 'panel',
+            items:{ html: 'Choisis ton kart: '}
+        };
+
+
+       var panel = {
+            xtype : 'panel',
+            layout: 'hbox',
+            align: 'center',
+
+            items: [
+                rouge, vert, bleu, jaune
+            ]
+        };
+        this.add([paneltext, panel]);
+    },
+    onChooseKartRouge : function(){
+        var text = "Tu as choisi le kart rouge" ;
+        console.log(text);
+        this.fireEvent("chooseKartRouge", this);
+    },
+    onVertTap : function(){
+        var text = "Tu as choisi le kart vert" ;
+        console.log(text);
+        this.fireEvent("onVertTap", this);
+    },
+    /*onChooseKartBleu : function(){
+        var text = "Tu as choisi le kart bleu" ;
+        console.log(text);
+        this.fireEvent("chooseKartBleu", this);
+    },*/
+   /* onChooseKartJaune : function(){
+        var text = "Tu as choisi le kart jaune" ;
+        console.log(text);
+        this.fireEvent("chooseKartJaune", this);
+    }*/
+
+});
+
+
+    /*items: [
+
+        {cls: 'painting kart1'},
+        {cls: 'painting kart2'},
+        {cls: 'painting kart3'},
+        {cls: 'painting kart4'}
+
+    ]*/
